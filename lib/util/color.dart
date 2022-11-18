a63deffa95a0cfcbdf6f107d1b8eb9ce4b556a13
Jacobyanmife <jacobyanmife@gmail.comm>
2022-11-18 23:56:35 +0100

    style: Implement style guide

--- lib/util/color.dart
+++ lib/util/color.dart
@@ -7,7 +7,8 @@ class AppColor {
   static const Color kErrorColor = Color(0xFFda2525);
   static const Color kGrayColor = Color(0xFF575757);
   static const Color kWarningColor = Color(0xFFe7b913);
-  
+
+  //Shades of Primary colors
   static const primaryColor = MaterialColor(
     0xff204e65,
     {
@@ -24,6 +25,7 @@ class AppColor {
     },
   );
 
+  //Shades of Secondary colors
   static const secondaryColor = MaterialColor(
     0XFF4FB1DC,
     {
@@ -37,6 +39,74 @@ class AppColor {
       700: Color(0xFF387e9c),
       800: Color(0xFF2b6179),
       900: Color(0xFF214a5c),
+    },
+  );
+
+  //Shades of gray colors
+  static const grayColor = MaterialColor(
+    0XFF575757,
+    {
+      50: Color(0XFFeeeeee),
+      100: Color(0XFFcbcbcb),
+      200: Color(0XFFb2b2b2),
+      300: Color(0XFF8e8e8e),
+      400: Color(0XFF797979),
+      500: Color(0XFF575757),
+      600: Color(0xFF4f4f4f),
+      700: Color(0xFF3e3e3e),
+      800: Color(0xFF303030),
+      900: Color(0xFF252525),
+    },
+  );
+
+  //Shades of gray colors
+  static const warningColor = MaterialColor(
+    0XFFe7b913,
+    {
+      50: Color(0XFFfdf8e7),
+      100: Color(0XFFf8e9b6),
+      200: Color(0XFFf4df92),
+      300: Color(0XFFefd061),
+      400: Color(0XFFecc742),
+      500: Color(0XFFe7b913),
+      600: Color(0xFFd2a811),
+      700: Color(0xFFa4830d),
+      800: Color(0xFF7f660a),
+      900: Color(0xFF614e08),
+    },
+  );
+
+  //Shades of error colors
+  static const errorColor = MaterialColor(
+    0XFFda2525,
+    {
+      50: Color(0XFFfbe9e9),
+      100: Color(0XFFf4bbbb),
+      200: Color(0XFFee9b9b),
+      300: Color(0XFFe66d6d),
+      400: Color(0XFFe15151),
+      500: Color(0XFFda2525),
+      600: Color(0xFFc62222),
+      700: Color(0xFF9b1a1a),
+      800: Color(0xFF781414),
+      900: Color(0xFF5c1010),
+    },
+  );
+
+  // Shades of success color
+  static const successColor = MaterialColor(
+    0XFF529f50,
+    {
+      50: Color(0XFFeef5ee),
+      100: Color(0XFFc9e1c9),
+      200: Color(0XFFafd3af),
+      300: Color(0XFF8bbf8a),
+      400: Color(0XFF75b273),
+      500: Color(0XFF529f50),
+      600: Color(0xFF4b9149),
+      700: Color(0xFF3a7139),
+      800: Color(0xFF2d572c),
+      900: Color(0xFF224322),
     },
   );
 }
