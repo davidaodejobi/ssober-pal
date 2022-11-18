3411117b3baab02cfded70473cf20c99fbb5df4a
Betrue <femigbajuola@gmail.com>
2022-11-18 15:20:53 +0100

    feat: added routes and erropage to it

--- lib/routes/route.dart
+++ lib/routes/route.dart
@@ -1,11 +1,13 @@
 import 'package:addictionsupportroom/routes/error404.dart';
 import 'package:addictionsupportroom/testingpage.dart';
+import 'package:addictionsupportroom/view/account/home.dart';
 import 'package:auto_route/auto_route.dart';
 
 @MaterialAutoRouter(
   routes: [
     MaterialRoute(page: TestPage, name: "testing", path: "/", initial: true),
     MaterialRoute(page: Error404Screen, name: "error404", path: "/error404"),
+    // MaterialRoute(page: AccountHome, name: "AccountHome", path: "/account"),
 //UnknownRoutePage here
     RedirectRoute(
       path: '*',
