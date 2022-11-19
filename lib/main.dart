5b970e32e5fee28a6d62f79fc617269e9d0bb5dd
Kanma <hchizea@gmail.com>
2022-11-19 20:38:16 +0100

    fix: add change notifier provider

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appRouter = AppRouter();
-    return MaterialApp.router(
+    return MultiProvider(
+providers:[
+ChangeNotifierProvider(create:(context)=> FeelingsController())
+]
+child: MaterialApp.router(
       routerDelegate: appRouter.delegate(),
       routeInformationParser: appRouter.defaultRouteParser(),
-    );
+    ));
   }
 }
