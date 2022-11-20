c8c43520345862c9ab12c28ab4a969a4c9d1b68b
Ajai Olufisayo <tadeajai4@gmail.com>
2022-11-20 13:02:32 +0100

    Merge remote-tracking branch 'origin/feat_notifications' into feat_notifications
    Signed-off-by: Ajai Olufisayo <tadeajai4@gmail.com>

--- lib/view/notifications/widgets/notifications_app_bar.dart
+++ lib/view/notifications/widgets/notifications_app_bar.dart
@@ -3,7 +3,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:addictionsupportroom/util/color.dart';
 
-import '../../../util/color.dart';
 
 AppBar buildAppBar(BuildContext context) {
   return AppBar(
