85289b2c0495fee264f0fff7f8b702035587edf5
jacobyanmife <62618625+jacobyanmife@users.noreply.github.com>
2022-11-20 12:18:04 +0100

    Style/ implementing style guide (#14)
    * feat: adding colour package
    
    * style: Implement style guide
    
    * feat: Implementing update on the style guide
    
    Co-authored-by: Jacobyanmife <jacobyanmife@gmail.comm>

---
+++ lib/util/spacing.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AppSpace {
+
+  //Vertical Spacing
+  static const vertical4 = SizedBox(height: 4);
+  static const vertical8 = SizedBox(height: 8);
+  static const vertical12 = SizedBox(height: 12);
+  static const vertical16 = SizedBox(height: 16);
+  static const vertical20 = SizedBox(height: 20);
+  static const vertical24 = SizedBox(height: 24);
+  static const vertical32 = SizedBox(height: 32);
+  static const vertical40 = SizedBox(height: 40);
+  static const vertical48 = SizedBox(height: 48);
+  static const vertical64 = SizedBox(height: 64);
+  static const vertical80 = SizedBox(height: 80);
+  static const vertical96 = SizedBox(height: 96);
+  static const vertical128 = SizedBox(height: 128);
+  static const vertical160 = SizedBox(height: 160);
+  static const vertical192 = SizedBox(height: 192);
+  static const vertical224 = SizedBox(height: 224);
+  static const vertical256 = SizedBox(height: 256);
+
+  //Horizontal Spacing
+  static const horizontal4 = SizedBox(width: 4);
+  static const horizontal8 = SizedBox(width: 8);
+  static const horizontal12 = SizedBox(width: 12);
+  static const horizontal16 = SizedBox(width: 16);
+  static const horizontal20 = SizedBox(width: 20);
+  static const horizontal24 = SizedBox(width: 24);
+  static const horizontal32 = SizedBox(width: 32);
+  static const horizontal40 = SizedBox(width: 40);
+  static const horizontal48 = SizedBox(width: 48);
+  static const horizontal64 = SizedBox(width: 64);
+  static const horizontal80 = SizedBox(width: 80);
+  static const horizontal96 = SizedBox(width: 96);
+  static const horizontal128 = SizedBox(width: 128);
+  static const horizontal160 = SizedBox(width: 160);
+  static const horizontal192 = SizedBox(width: 192);
+  static const horizontal224 = SizedBox(width: 224);
+  static const horizontal256 = SizedBox(width: 256);
+}
